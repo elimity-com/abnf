d490909b56f6ea99ce24b6308d45d3063456c1cd
di-wu <me@di-wu.be>
2020-03-21 10:36:29 +0100

    Fix stack overflow

--- abnf_definition_test.go
+++ abnf_definition_test.go
@@ -1,6 +1,7 @@
 package abnf
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/di-wu/regen"
@@ -8,44 +9,90 @@ import (
 
 func TestDefinition(t *testing.T) {
 	for _, test := range []struct {
+		name     string
+		rule     Operator
+		examples []string
+	}{
+		{
+			name: "ruleName",
+			rule: ruleName,
+			examples: []string{
+				"name",
+				`name123`,
+				`name1-2-3`,
+			},
+		},
+		{
+			name: "definedAs",
+			rule: definedAs,
+			examples: []string{
+				" = ",
+				"=/",
+			},
+		},
+		{
+			name: `element`,
+			rule: element,
+			examples: []string{
+				`rule-name`, // rule name
+				`( %x01 )`,  // group
+				`[ %x01 ]`,  // option
+				`"charval"`, // char value
+				`%x01`,      // numerical value
+				`<abc>`,     // prose value
+			},
+		},
+	} {
+		for _, s := range test.examples {
+			t.Run(fmt.Sprintf("%s %s", test.name, s), func(t *testing.T) {
+				if value := ParseString(s, test.rule); value == nil {
+					t.Errorf("no value found for: %s", s)
+				}
+			})
+		}
+	}
+}
+
+func TestValues(t *testing.T) {
+	for _, test := range []struct {
 		name                     string
 		validRegex, invalidRegex string
-		rule                     RuleFunc
+		rule                     Operator
 	}{
 		{
 			name:         "CharVal",
 			validRegex:   `"[a-zA-Z]"`,
 			invalidRegex: `[a-zA-Z]`,
-			rule:         charVal(),
+			rule:         charVal,
 		},
 		{
 			name:       "NumVal",
 			validRegex: `%((b[0-1]+(.[0-1]+|-[0-1]+)?)|(d\d+(.\d+|-\d+)?)|(x[0-9A-F]+(.[0-9A-F]+|-[0-9A-F]+)?))`,
-			rule:       numVal(),
+			rule:       numVal,
 		},
 		{
 			name:         "BinVal",
 			validRegex:   `b[0-1]+(.[0-1]+|-[0-1]+)?`,
 			invalidRegex: `[0-1]+(.[0-1]+|-[0-1]+)?`,
-			rule:         binVal(),
+			rule:         binVal,
 		},
 		{
 			name:         "DecVal",
 			validRegex:   `d\d+(.\d+|-\d+)?`,
 			invalidRegex: `\d+(.\d+|-\d+)?`,
-			rule:         decVal(),
+			rule:         decVal,
 		},
 		{
 			name:         "HexVal",
 			validRegex:   `x[0-9A-F]+(.[0-9A-F]+|-[0-9A-F]+)?`,
 			invalidRegex: `[0-9A-F]+(.[0-9A-F]+|-[0-9A-F]+)?`,
-			rule:         hexVal(),
+			rule:         hexVal,
 		},
 		{
 			name:         "ProseVal",
 			validRegex:   `<[a-zA-Z]*>`,
 			invalidRegex: `[a-zA-Z]*`,
-			rule:         proseVal(),
+			rule:         proseVal,
 		},
 	} {
 		t.Run(test.name, func(t *testing.T) {
--- core_abnf.go
+++ core_abnf.go
@@ -3,83 +3,83 @@ package abnf
 // RFC 5234: Appendix B. Core ABNF of ABNF
 
 // ALPHA = %x41-5A / %x61-7A ; A-Z / a-z
-func alpha() RuleFunc {
+func alpha() Operator {
 	return Alts(Range('\x41', '\x5A'), Range('\x61', '\x7A'))
 }
 
 // BIT = "0" / "1"
-func bit() RuleFunc {
+func bit() Operator {
 	return Runes('0', '1')
 }
 
 // CHAR = %x01-7F ; any 7-bit US-ASCII character, excluding NUL
-func char() RuleFunc {
+func char() Operator {
 	return Range('\x01', '\x7F')
 }
 
 // CR = %x0D ; carriage return
-func cr() RuleFunc {
+func cr() Operator {
 	return Rune('\x0D')
 }
 
 // CRLF = CR LF ; Internet standard newline
-func crlf() RuleFunc {
+func crlf() Operator {
 	return Concat(cr(), lf())
 }
 
 // CTL = %x00-1F / %x7F ; controls
-func ctl() RuleFunc {
+func ctl() Operator {
 	return Alts(Range('\x00', '\x1F'), Rune('\x7F'))
 }
 
 // DIGIT = %x30-39 ; 0-9
-func digit() RuleFunc {
+func digit() Operator {
 	return Range('\x30', '\x39')
 }
 
 // DQUOTE = %x22 ; " (Double Quote)
-func dquote() RuleFunc {
+func dquote() Operator {
 	return Rune('\x22')
 }
 
 // HEXDIG = DIGIT / "A" / "B" / "C" / "D" / "E" / "F"
-func hexdig() RuleFunc {
+func hexdig() Operator {
 	return Alts(digit(), Rune('A'), Rune('B'), Rune('C'), Rune('D'), Rune('E'), Rune('F'))
 }
 
 // HTAB = %x09 ; horizontal tab
-func htab() RuleFunc {
+func htab() Operator {
 	return Rune('\x09')
 }
 
 // LF = %x0A ; linefeed
-func lf() RuleFunc {
+func lf() Operator {
 	return Rune('\x0A')
 }
 
 // LWSP = *(WSP / CRLF WSP) ; Use of this linear-white-space rule permits lines containing only white space that are
 // no longer legal in mail headers and have caused interoperability problems in other contexts. Do not use when
 // defining mail headers and use with caution in other contexts.
-func lwsp() RuleFunc {
+func lwsp() Operator {
 	return Repeat0Inf(Alts(wsp(), Concat(crlf(), wsp())))
 }
 
 // OCTET = %x00-FF ; 8 bits of data
-func octet() RuleFunc {
+func octet() Operator {
 	return Range('\x00', '\xFF')
 }
 
 // SP = %x20
-func sp() RuleFunc {
+func sp() Operator {
 	return Rune('\x20')
 }
 
 // VCHAR = %x21-7E ; visible (printing) characters
-func vchar() RuleFunc {
+func vchar() Operator {
 	return Range('\x21', '\x7E')
 }
 
 // WSP = SP / HTAB ; white space
-func wsp() RuleFunc {
+func wsp() Operator {
 	return Alts(sp(), htab())
 }
