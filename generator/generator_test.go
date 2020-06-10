f29a09df5c6881888daa8aafd1727c929ca4e0ac
Quint Daenen <me@di-wu.be>
2020-06-10 10:21:22 +0200

    Add generator test for core package

--- generator/generator_test.go
+++ generator/generator_test.go
@@ -3,16 +3,41 @@ package generator
 import (
 	"fmt"
 	"io/ioutil"
+	"strings"
 	"testing"
 )
 
 func TestGenerateCore(t *testing.T) {
-	raw, _ := ioutil.ReadFile("../testdata/core.abnf")
-	f := GenerateABNF("core", string(raw))
+	rawABNF, err := ioutil.ReadFile("../testdata/core.abnf")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	f := GenerateABNF("core", string(rawABNF))
+	raw, err := ioutil.ReadFile("../core/core_abnf.go")
+	if err != nil {
+		t.Error(err)
+		return
+	}
 
-	fmt.Printf("%#v", f)
-
-	return
-
-	_ = ioutil.WriteFile("../core/core_abnf.go",  []byte(fmt.Sprintf("%#v", f)), 0644)
+	// NOTE: 1-index based line/position numbers!
+	originalSplit := strings.Split(fmt.Sprintf("%#v", f), "\n")
+	generatedSplit := strings.Split(string(raw), "\n")
+	if len(originalSplit) != len(generatedSplit) {
+		t.Error("no equal amount of lines")
+		return
+	}
+	for i := range originalSplit {
+		if originalSplit[i] != generatedSplit[i] {
+			if len(originalSplit[i]) != len(generatedSplit[i]) {
+				t.Errorf("no equal amount of characters on line %d", i+1)
+				return
+			}
+			for j := range originalSplit[i] {
+				if originalSplit[i][j] != generatedSplit[i][j] {
+					t.Errorf("line %d: characters do not match at position %d", i+1, j+1)
+				}
+			}
+		}
+	}
 }
